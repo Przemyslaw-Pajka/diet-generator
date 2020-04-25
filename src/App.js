06ce64b6a13916eda3d9c84d6bd626597d7870f3
Przemek <przemyslaw.pajka94@gmail.com>
2020-04-25 15:44:08 +0200

    Repair Router history, add basename

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import { BrowserRouter as Router, Redirect, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Cockpit from "./components/Cockpit/Cockpit";
 import { MealStatesProvider } from "./store/MealStates";
+import createHistory from "history/createBrowserHistory";
 
+const history = createHistory({
+  basename: process.env.PUBLIC_URL,
+});
 class App extends Component {
   constructor(props) {
     super(props);
@@ -12,7 +16,7 @@ class App extends Component {
   render() {
     return (
       <MealStatesProvider>
-        <Router>
+        <Router history={history} basename={process.env.PUBLIC_URL}>
           <Route exact path="/">
             <Redirect to="/home" />
           </Route>
--- src/modules/getMealsData/getMealsData.js
+++ src/modules/getMealsData/getMealsData.js
@@ -1,20 +1,20 @@
 const getMeals = () => {
-    return new Promise((resolve,reject) => {
-        let xmlhttp = new XMLHttpRequest();
-        let obj;
-        xmlhttp.open('GET',`${window.location.origin}/data.json`);
-        xmlhttp.send();
-        xmlhttp.onreadystatechange = function (aEvt) {
-            if (xmlhttp.readyState == 4) {
-                if(xmlhttp.status == 200) {
-                    obj = JSON.parse(JSON.parse(JSON.stringify(xmlhttp.responseText)));
-                    resolve (obj);     
-                }    
-                if(xmlhttp.status == 404) {
-                    reject(new Error(xmlhttp.statusText));  
-                }  
-            }
-        };
-    })
-}
+  return new Promise((resolve, reject) => {
+    let xmlhttp = new XMLHttpRequest();
+    let obj;
+    xmlhttp.open("GET", `${process.env.PUBLIC_URL}/data.json`);
+    xmlhttp.send();
+    xmlhttp.onreadystatechange = function (aEvt) {
+      if (xmlhttp.readyState == 4) {
+        if (xmlhttp.status == 200) {
+          obj = JSON.parse(JSON.parse(JSON.stringify(xmlhttp.responseText)));
+          resolve(obj);
+        }
+        if (xmlhttp.status == 404) {
+          reject(new Error(xmlhttp.statusText));
+        }
+      }
+    };
+  });
+};
 export default getMeals;
