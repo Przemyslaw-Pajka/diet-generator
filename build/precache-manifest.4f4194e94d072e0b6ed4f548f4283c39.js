4c50efcb98aacb25a58d590b8513e109c5d9a91c
Przemek <przemyslaw.pajka94@gmail.com>
2020-04-27 16:22:57 +0200

    Fix generateContainer width on mobile, fix item's width in modal for better displaying on mobile

---
+++ build/precache-manifest.4f4194e94d072e0b6ed4f548f4283c39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80ae6ee98a6be9ed48d710ef46b45e3f",
+    "url": "/diet-generator/index.html"
+  },
+  {
+    "revision": "82189e19af0b8115f79e",
+    "url": "/diet-generator/static/css/main.bc6cb39a.chunk.css"
+  },
+  {
+    "revision": "7c7817a219771cc9f727",
+    "url": "/diet-generator/static/js/2.f9726655.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/diet-generator/static/js/2.f9726655.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82189e19af0b8115f79e",
+    "url": "/diet-generator/static/js/main.f776e946.chunk.js"
+  },
+  {
+    "revision": "162bce5662420c842a24",
+    "url": "/diet-generator/static/js/runtime-main.88f7cfe8.js"
+  }
+]);
